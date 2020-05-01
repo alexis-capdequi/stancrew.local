3c1df4b9a94a23cfb2d035971a0e3ea3d653d412
alexis <alexis.capdequi@gmail.com>
2020-05-02 00:18:10 +0200

    Create Video part

---
+++ src/Controller/VideoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\VideoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("videos")
+ */
+class VideoController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_videos")
+     * @return Response
+     */
+    public function index(VideoRepository $videoRepository): Response
+    {
+        return $this->render('videos/list.html.twig', [
+            'videos' => $videoRepository->findAllOrderByIdDesc(),
+        ]);
+    }
+    
+    /**
+     * @Route("/{type}", name="app_videos_bytype", requirements={"type"="previews|clips|concerts"}))
+     * @return Response
+     */
+    public function videosByType(string $type, VideoRepository $videoRepository): Response
+    {
+        $videos = $videoRepository->findBy(
+            array('type' => $type),
+            array('id' => 'desc')
+        );
+        
+        return $this->render('videos/list.html.twig', [
+            'videos' => $videos,
+        ]);
+    }
+    
+    /**
+     * @Route("/watch", name="app_videos_watch")
+     * @return Response
+     */
+    public function videoWatch(Request $request, VideoRepository $videoRepository): Response
+    {
+        $type = $request->get('t');
+        $uniqid = $request->get('v');
+        
+        if (isset($type) && !empty($type)) {
+            $videos = $videoRepository->findBy(
+                array('type' => $type),
+                array('id' => 'desc')
+            );
+        } else {
+            $videos = $videoRepository->findAllOrderByIdDesc();
+        }
+        
+        $video = $videoRepository->findOneBy(
+            array('uniqid' => $uniqid)
+        );
+        
+        return $this->render('videos/watch.html.twig', [
+            'videos' => $videos,
+            'video' => $video
+        ]);
+    }
+}
---
+++ src/Entity/Video.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VideoRepository")
+ */
+class Video
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $uniqid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $publicationDate;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $file;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUniqid(): ?string
+    {
+        return $this->uniqid;
+    }
+
+    public function setUniqid(string $uniqid): self
+    {
+        $this->uniqid = $uniqid;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(\DateTimeInterface $publicationDate): self
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+}
